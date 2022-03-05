fc4da60cfdcf76d1052b926f4ac40e503e744185
Sarat Chandra Muraharirao <sarat.muraharirao@gmail.com>
2022-03-05 23:12:09 +0530

    check husky

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-
+import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+
 import { BrowserRouter } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals'
 
